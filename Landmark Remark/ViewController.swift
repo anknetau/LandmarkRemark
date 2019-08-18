609b36683d376b331c495f30cf71e02eeee3ee3f
Andres Kievsky <andres.kievsky@gmail.com>
2019-08-18 14:14:33 +1000

    Create initial project

---
+++ Landmark Remark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Landmark Remark
+//
+//  Created by Andres Kievsky on 18/8/19.
+//  Copyright © 2019 Andres Kievsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
